eb79e9e4fb89a4712e2ed6490f646af8a8143291
adrienbaron <adrien.baron+kf@theappbusiness.com>
2019-04-17 11:00:15 +0100

    add a test for 2 + 2

--- src/sum.test.js
+++ src/sum.test.js
@@ -5,5 +5,9 @@ test('adds 1 + 2 to equal 3', () => {
 });
 
 test('adds 4 + 2 to equal 6', () => {
-  expect(sum(4, 2)).toBe(2);
+  expect(sum(4, 2)).toBe(6);
+});
+
+test('adds 2 + 2 to equal 4', () => {
+  expect(sum(2, 2)).toBe(4);
 });
