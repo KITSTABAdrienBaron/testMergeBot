901e096a1038b043801bbd00ac89c4a09e2f9d33
adrienbaron <adrien.baron+kf@theappbusiness.com>
2019-04-17 14:43:58 +0100

    cleanup create commits

--- createCommits.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-touch LOTS_COMMITS.md
-git add LOTS_COMMITS.md
-for i in {6..100}; do echo "Hello $i" >> LOTS_COMMITS.md && git commit -a -m "commit $i"; done
--- createCommits2.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-touch LOTS_COMMITS_2.md
-git add LOTS_COMMITS_2.md
-for i in {1..100}; do echo "Hello $i" >> LOTS_COMMITS_2.md && git commit -a -m "commit $i"; done
